2abc57a04a71cf9017e64469d1bd17e93fb0e6fa
Litvinov Maxim <164255313+litvinovmaxim@users.noreply.github.com>
2024-06-09 17:28:31 +0300

    Add files via upload

---
+++ service/footer.js
@@ -0,0 +1,26 @@
+let Footer = `
+<div class="footer">
+    Встречи на кухне. Максим Литвинов, 2024.
+    <div class="social-icons">
+        <a href="https://vk.com/hewipa" target="_blank">
+            <img src="images/special/vk.svg" alt="Вконтакте">
+        </a>
+
+        <a href="https://t.me/hewipa" target="_blank">
+            <img src="images/special/telegram.svg" alt="Telegram">
+        </a>
+
+        <a href="https://www.chsu.ru" target="_blank">
+            <img src="images/special/chsu.svg" alt="ЧГУ">
+        </a>
+        <a href="https://github.com/litvinovmaxim/" target="_blank" class="animate">
+            <img src="images/special/github.svg" alt="GitHub">
+        </a>
+
+        <a href="#" target="">
+            <img src="images/special/up.svg" alt="UP">
+        </a>
+    </div>
+</div>
+`;
+document.getElementById("footer-site").innerHTML = Footer;
